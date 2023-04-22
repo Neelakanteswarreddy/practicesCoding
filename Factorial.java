ac6a1b918d225f3f07e142d0fd752240bb860a54
Neelakanteswarreddy <neelakanteswarreddy0205@gmail.com>
2023-04-22 10:50:24 +0530

    Code is Updated with main method

--- Factorial.java
+++ Factorial.java
@@ -1,11 +1,19 @@
 class Factorial{
- public static int Fact(int x)
- {
-     int fact=1;
-    for (int i = 0; i <=x; i++) 
-       {
-  	    fact=fact*x;
-  	 }
-	     return fact;
- }
+    public static int Fact(int x)
+     {
+          int fact=1;
+        for (int i = 0; i <=x; i++) 
+         {
+  	      fact=fact*x;
+  	   }
+	       return fact;
+     }
+ public static void main(String... args)
+  {
+    int a=10;
+     int s=Fact(a);
+     System.out.println(s);
+   }
+}
+
    
